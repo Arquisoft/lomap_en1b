d30852ff78b4a553f5d2b87a0b96f1e5eea40109
Mario Pérez <mariosoiram1@gmail.com>
2023-03-05 21:23:49 +0100

    404 example page

--- webapp/src/routes/404error.tsx
+++ webapp/src/routes/404error.tsx
@@ -1,10 +1,42 @@
+import styled from "@emotion/styled";
+import {Button} from "@chakra-ui/react";
+import {useNavigate} from "react-router-dom";
 
 function Error404() {
+    const navigate = useNavigate()
     return(
-        <h1>404 ERROR - Not FOUND</h1>
-
+        <ScreenContainer>
+            <div>
+                <h1>404 Not found</h1>
+                <p>Got lost? No worries...</p>
+                <Button colorScheme='teal' onClick={() => navigate("/")}>Go home</Button>
+            </div>
+        </ScreenContainer>
     );
 
 }
 
+
 export default Error404;
+
+
+const ScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  
+  & > div {
+    width: 100vw;
+    max-width: 400px;
+    padding: 2rem;
+    border-radius: 1em;
+    border: 1px solid gray;
+    
+    & > h1 {
+      font-size: xxx-large;
+    }
+  }
+`
