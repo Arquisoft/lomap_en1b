90b768178989962d509b5890f73973003f4ba732
uo276188 <uo276188@uniovi.es>
2023-03-06 11:27:41 +0100

    Cleaning imports in App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; //package for routes
 import LoginForm from './components/Login';
 
-import Index from "./routes";
 import Error404 from "./routes/404error";
 import HomePage from './components/Home';
 import AboutPage from './components/About';
@@ -15,6 +13,7 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element:<LoginForm />,
+    errorElement: <Error404 />,
   },
   {
     path: "/",
