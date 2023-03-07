137123625b067da2a4572a5293a865c28b9c8f7d
UO278249 <danaam2001@gmail.com>
2023-03-07 22:16:44 +0100

    Changes made to mapview

--- webapp/src/mapView/MapView.tsx
+++ webapp/src/mapView/MapView.tsx
@@ -1,30 +1,41 @@
 import styled from "@emotion/styled";
-import React, { Component,useState} from 'react';
+import React, { Component,useState,useEffect } from 'react';
 import L from 'leaflet';
 import { useDispatch, useSelector } from 'react-redux'
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, useMap ,Marker,useMapEvents,Popup} from 'react-leaflet';
 import PropTypes from 'prop-types';
 
-const dispatch = useDispatch();
 const markersLayer = L.featureGroup();
 
 
 function MapView(){
+    const dispatch = useDispatch();
 
-    var map = L.map('map', 
-    {center: [-5.8447600,  43.3602900],
-    zoomControl: true,
-    zoom: 12});
+    useEffect(() => {
+        const map = L.map('map',
+            {
+                center: [-5.8447600, 43.3602900],
+                zoomControl: true,
+                zoom: 12
+            });
 
-    L.control
-      .zoom({
-        position: 'topright'
-      })
-      .addTo(map)
+        L.control
+            .zoom({
+                position: 'topright'
+            })
+            .addTo(map)
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
+    });
     return (
-      <ScreenContainer id="map"/>
+      <ScreenContainer>
+          <div>a</div>
+          <MapContainer id={'map'}>
+          </MapContainer>
+      </ScreenContainer>
     )
 }
 
