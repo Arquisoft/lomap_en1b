a0112c8f3b5820d33819d96edc3fd42e1787fa10
Sara <SARA@LAPTOP-EEOJ13BM>
2023-03-09 00:50:11 +0100

    Map with one click marker

---
+++ webapp/src/components/Map.tsx
@@ -0,0 +1,58 @@
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents, useMapEvent } from 'react-leaflet';
+import React, { useState } from 'react';
+import { Flex, Stack } from '@chakra-ui/react';
+import "../css/react_leaflet.css";
+import 'leaflet/dist/leaflet.css';
+import { LatLng, LatLngExpression } from 'leaflet';
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from 'leaflet';
+
+export function LocationMarker() {
+    // const [position, setPosition] : LatLng = {lat: 0, lng: 0};
+    const [lati, setLat] = useState(0);
+    const [lngi, setLng] = useState(0);
+    
+    const map = useMapEvents({
+        click: (e) => {
+            setLat(e.latlng.lat);
+            setLng(e.latlng.lng);
+
+            console.log(e.latlng);
+        },
+    })
+
+
+    return  (
+        <Marker position={[lati, lngi]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [30, 45], iconAnchor: [10, 40] })}>
+            <Popup>You are here</Popup>
+        </Marker>
+    )
+}
+
+
+export default function MapElement(): JSX.Element {
+    const escuela: LatLngExpression = { lat: 43.354, lng: -5.851 };
+    return (
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}>
+
+            <Stack>
+                <MapContainer center={escuela} zoom={13} scrollWheelZoom={true}>
+                    <LocationMarker/>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={escuela} icon={new Icon({ iconUrl: markerIconPng, iconSize: [30, 45], iconAnchor: [10, 40] })}>
+                        <Popup>
+                            School of Software Engineering <br /> Univrsidad de Oviedo.
+                        </Popup>
+                    </Marker>
+
+                </MapContainer>
+            </Stack>
+        </Flex>
+    );
+}
