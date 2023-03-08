2763e33e46d9d6b67dc8054b85bd41875165c03e
Elías Llera García-Riaño <eliaslleragr@gmail.com>
2023-03-08 23:47:27 +0100

    Included location type

--- restapi/src/builders/locationBuilder.ts
+++ restapi/src/builders/locationBuilder.ts
@@ -1,5 +1,5 @@
 import {buildThing, createThing, getDecimal, getStringNoLocale, Thing} from "@inrupt/solid-client";
-import {Location} from "../types";
+import {Location, LocationType} from "../types";
 import {RDF, SCHEMA_INRUPT} from "@inrupt/vocab-common-rdf";
 
 export function thingToLocation(locationThing:Thing) : Location {
@@ -7,6 +7,7 @@ export function thingToLocation(locationThing:Thing) : Location {
     return {
         url: locationThing.url,
         name: getStringNoLocale(locationThing, SCHEMA_INRUPT.name)!,
+        locationType: getStringNoLocale(locationThing, SCHEMA_INRUPT.description)! as LocationType,
         latitude: getDecimal(locationThing, SCHEMA_INRUPT.latitude)!,
         longitude: getDecimal(locationThing, SCHEMA_INRUPT.longitude)!
     }
@@ -15,6 +16,7 @@ export function thingToLocation(locationThing:Thing) : Location {
 export function locationToThing(location:Location):Thing{
     return buildThing(createThing({name: "Location1"}))
         .addStringNoLocale(SCHEMA_INRUPT.name, location.name)
+        .addStringNoLocale(SCHEMA_INRUPT.description, location.locationType)
         .addDecimal(SCHEMA_INRUPT.latitude, location.latitude)
         .addDecimal(SCHEMA_INRUPT.longitude, location.longitude)
         .addUrl(RDF.type, "https://schema.org/Place")
--- restapi/src/types.d.ts
+++ restapi/src/types.d.ts
@@ -1,6 +1,9 @@
 export type Location = {
     url:string,
     name:string,
+    locationType:LocationType,
     latitude:number,
     longitude:number
-}
+};
+
+export type LocationType = 'shop' | 'bar' | 'restaurant' | 'sight' | 'monument';
