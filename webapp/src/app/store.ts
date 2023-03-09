15cbfeb05dda9913a4d4b33e74cdbb1ff70a6cc5
Mario Pérez <mariosoiram1@gmail.com>
2023-03-09 12:09:11 +0100

    Login with pods (waiting for RestAPI cookie changes)

--- webapp/src/app/store.ts
+++ webapp/src/app/store.ts
@@ -1,15 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import {authApi} from "./services/Auth";
 import {locationApi} from "./services/Location";
 
 export const store = configureStore({
   reducer: {
-    [authApi.reducerPath]: authApi.reducer,
     [locationApi.reducerPath]: locationApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(
-          authApi.middleware,
           locationApi.middleware
       ),
 });
