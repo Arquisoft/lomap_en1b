dc1c0ab3b31108329e89fe5c722f6c6f43aa2f77
Iván Vega García <uo276670@uniovi.es>
2023-03-10 11:37:52 +0100

    Location Slice for testing without the API

--- webapp/src/app/services/Location.tsx
+++ webapp/src/app/services/Location.tsx
@@ -1,4 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import {createSelector, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {RootState} from "../store";
 //import type { Location } from './types' //TODO: Importar el tipo
 
 export const locationApi = createApi({
@@ -8,7 +10,35 @@ export const locationApi = createApi({
         getLocations: builder.query<Location, string>({
             query: (name) => `location/`,
         }),
+        //TODO: Check if this makes any sense
+        addLocation: builder.mutation<void, Omit<Location, 'id'>>({
+            query: (newLocation) => ({
+                url: `/locations`,
+                method: 'POST',
+                body: newLocation,
+            })
+        }),
     }),
 })
 
+//I created a Slice to store the location in the store so that we can test without using the API
+interface LocationsState {
+    locations: Location[];
+}
+const initialState: LocationsState = {
+    locations: [],
+};
+
+const locationsSlice = createSlice({
+    name: 'locationStore',
+    initialState,
+    reducers: {
+        addLocation: (state, action: PayloadAction<Location>) =>{
+            state.locations.push(action.payload);
+        }
+    },
+});
+
+
 export const {useGetLocationsQuery} = locationApi
+export  const {addLocation} = locationsSlice.actions;
