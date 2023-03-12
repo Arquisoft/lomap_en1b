02492fa0f05a5d8d9b46cbdbc45e80a266b723f9
Sara <SARA@LAPTOP-EEOJ13BM>
2023-03-12 13:48:12 +0100

    addign ivan's modifications, error in counterslice

--- webapp/src/components/Map.tsx
+++ webapp/src/components/Map.tsx
@@ -6,7 +6,86 @@ import 'leaflet/dist/leaflet.css';
 import { LatLng, LatLngExpression } from 'leaflet';
 import markerIconPng from "leaflet/dist/images/marker-icon.png";
 import { Icon } from 'leaflet';
+import {useSelector, useDispatch} from 'react-redux';
+import type { MapMarker } from '../app/services/types';
+import {addLocation, selectAllLocations} from "../app/services/Location";
 
+
+
+
+
+
+export function LocationMarkerWithStore() {
+    // const [position, setPosition] : LatLng = {lat: 0, lng: 0};
+    const dispatch = useDispatch();
+//    const [lati, setLat] = useState(0);
+//    const [lngi, setLng] = useState(0);
+
+    const map = useMapEvents({
+        click: (e) => {
+            //setLat();
+            //setLng(e.latlng.lng);
+
+            //Create the type
+            const locMarker : MapMarker = {name : "", lat : e.latlng.lat, lng : e.latlng.lng, id: e.latlng.lat + " - " + e.latlng.lng};
+            //Store the location
+            dispatch(addLocation(locMarker)); // dispatch executes a reducer or action 
+        },
+
+    })
+
+    //Get all the locations
+    const locations = useSelector(selectAllLocations);
+
+    //Use .map to iterate and generate the corresponding markers
+    //This need to be optimiced because I think it generates again
+    //all the markers on top of each other
+    return  (
+        <div>
+            {locations.map(location => (
+                <Marker key={location.id} position={[location.lat, location.lng]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [30, 45], iconAnchor: [10, 40] })}>
+                    <Popup>You are here</Popup>
+                </Marker>
+            ))}
+        </div>
+
+    );
+}
+
+
+
+
+export default function MapElement(): JSX.Element {
+    const escuela: LatLngExpression = { lat: 43.354, lng: -5.851 };
+    return (
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}>
+
+            <Stack>
+                <MapContainer center={escuela} zoom={13} scrollWheelZoom={true}>
+                    <LocationMarkerWithStore/>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={escuela} icon={new Icon({ iconUrl: markerIconPng, iconSize: [30, 45], iconAnchor: [10, 40] })}>
+                        <Popup>
+                            School of Software Engineering <br /> Universidad de Oviedo.
+                        </Popup>
+                    </Marker>
+
+                </MapContainer>
+            </Stack>
+        </Flex>
+    );
+}
+
+
+
+
+/*
 type Marcador = {
     latitude: number;
     longitud: number;
@@ -44,7 +123,6 @@ export function LocationMarker() {
 
     return null;
 }
-
 
 export default function MapElement(): JSX.Element {
     const escuela: LatLngExpression = { lat: 43.354, lng: -5.851 };
@@ -91,8 +169,9 @@ export default function MapElement(): JSX.Element {
                         TODO must be replaced with PODS
                         No se updatea cuando se añade un marcador :( 
                             REDUX dispatch???
-                    */}
-
+                    */
+                   
+                   /*     }
                     {listaMarcadores.length > 0 && (
                         listaMarcadores.map( (marcador : Marcador) =>
                             (
@@ -110,3 +189,4 @@ export default function MapElement(): JSX.Element {
         </Flex>
     );
 }
+*/
