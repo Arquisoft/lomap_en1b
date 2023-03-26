7c9720882ed4d1d217f6c491b68bc8fb02abbe54
Elías Llera García-Riaño <eliaslleragr@gmail.com>
2023-03-26 13:55:19 +0200

    First implementation, needs testing

--- restapi/src/builders/friendBuilder.ts
+++ restapi/src/builders/friendBuilder.ts
@@ -1,12 +1,20 @@
-import {Friend, Location} from "../types";
-import {Thing} from "@inrupt/solid-client";
+import {Friend} from "../types";
+import {buildThing, createThing, getStringNoLocale, getUrl, Thing} from "@inrupt/solid-client";
+import {FOAF, RDF, RDFS} from "@inrupt/vocab-common-rdf";
 
-export function thingToFriend(_friendThing:Thing): Location {
-    //TODO
-    return undefined
+export function thingToFriend(friendThing:Thing): Friend {
+    return {
+        podId: getStringNoLocale(friendThing, FOAF.nick)!,
+        username: getUrl(friendThing, RDFS.seeAlso)!,
+    }
 }
 
-export function friendToThing(_friend:Friend): Thing{
+export function friendToThing(friend:Friend): Thing{
+    //Check how to work with knows
     //TODO
-    return null
+    return buildThing(createThing({name:friend.podId}))
+        .addStringNoLocale(FOAF.nick, friend.username)
+        .addUrl(RDFS.seeAlso, friend.podId)
+        .addUrl(RDF.type, "http://xmlns.com/foaf/0.1/#term_Person")
+        .build()
 }
