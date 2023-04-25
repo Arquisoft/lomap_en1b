d9ad184895c7027338681da34f06ce4770e09d56
Iván Vega García <uo276670@uniovi.es>
2023-04-26 00:26:37 +0200

    Fixed url on Reviews Hook

--- webapp/src/app/services/Reviews.tsx
+++ webapp/src/app/services/Reviews.tsx
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import {Review} from "../../types";
 
 export const reviewApi = createApi({
-    reducerPath: 'locationsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://4255db99-902a-464f-aa3a-e89ffa1a77fe.mock.pstmn.io' }),
+    reducerPath: 'review',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8082/' }),
     endpoints: (builder) => ({
         getReviews: builder.query<Review[], void>({
             query: (locationID) => ({
