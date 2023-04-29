32dbd204a8f0d56eaaa3ccb3911c9e1b66429908
Iván Vega García <uo276670@uniovi.es>
2023-04-29 23:50:30 +0200

    QuickFix: Hardcoded the url start for the reviewToThing function. Need to check if there are better alternatives

--- restapi/src/builders/reviewBuilder.ts
+++ restapi/src/builders/reviewBuilder.ts
@@ -6,7 +6,8 @@ import {RDF, RDFS, SCHEMA_INRUPT} from "@inrupt/vocab-common-rdf";
 export function reviewToThing(review:Review, authorWebId:string, imageUrl:string):Thing{
     return buildThing(createThing())
         .addStringNoLocale("https://schema.org/author", authorWebId)
-        .addUrl(RDFS.seeAlso, review.markerId)
+        //TODO: We need to check if this is the only url possible or if we need to check for the provider url
+        .addUrl(RDFS.seeAlso, "https://inrupt.com/.well-known/sdk-local-node/" + review.markerId)
         .addStringNoLocale(SCHEMA_INRUPT.text, review.comment)
         .addUrl(SCHEMA_INRUPT.image, imageUrl)
         .addInteger("https://schema.org/ratingValue", review.score)
