b2fce6753b66999e4e30812489c754fa85fec94b
Mario Pérez <mariosoiram1@gmail.com>
2023-05-02 02:16:23 +0200

    Map test Markers working

--- webapp/src/__tests__/Map.test.tsx
+++ webapp/src/__tests__/Map.test.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
-import { fireEvent, screen } from '@testing-library/react'
 import {render} from "../setupTests";
-import LoginForm from "../components/Login";
 import MapElement from "../components/Map";
+import {rest} from "msw";
+import {setupServer} from "msw/node";
+import {waitFor} from "@testing-library/react";
 
-export const handlers = [
+
+const handlers = [
     rest.get('http://localhost:8082/location', (req, res, ctx) => {
         return res(
+            ctx.status(200),
             ctx.json([
                 {
-                    id: 'Test',
-                    name: 'Test1',
-                    locationType: 'restaurant',
-                    latitude: 43.365143614762374,
-                    longitude: -5.851593017578125,
+                    id: '068b0c8a-fac2-4a46-b40c-07d74ccd46f7',
+                    name: 'Test',
+                    locationType: 'shop',
+                    latitude: 43.36136284334129,
+                    longitude: -5.851278233874803,
                     isShared: false,
                     isOwnLocation: false
                 },
                 {
-                    id: 'Test2',
+                    id: 'Test',
                     name: 'Test2',
                     locationType: 'restaurant',
                     latitude: 43.365143614762374,
@@ -28,27 +29,26 @@ export const handlers = [
                     isShared: false,
                     isOwnLocation: false
                 }
-            ])
+            ]),
         )
-    })
+    }),
 ]
 
 const server = setupServer(...handlers)
 
-// Enable API mocking before tests.
 beforeAll(() => server.listen())
-
-// Reset any runtime request handlers we may add during the tests.
 afterEach(() => server.resetHandlers())
-
-// Disable API mocking after the tests are done.
 afterAll(() => server.close())
+
 
 describe("Map tests", () =>{
     test('Renders the response from restAPI', async () => {
         const { container } = render(<MapElement />)
-        const markers = container.querySelectorAll(".leaflet-marker-icon")
-        expect(markers.length).toBe(2);
+        await waitFor(() => {
+            const markers = container.querySelectorAll('.leaflet-marker-icon');
+            expect(markers.length).toBe(2);
+        });
+
     })
 
 })
