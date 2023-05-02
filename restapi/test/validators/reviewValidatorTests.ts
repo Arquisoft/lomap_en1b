a29675caeb817484f6da654b4ec63c0543f93b53
Elías Llera García-Riaño <eliaslleragr@gmail.com>
2023-05-02 16:23:28 +0200

    First test implementation for the restAPI

---
+++ restapi/test/validators/reviewValidatorTests.ts
@@ -0,0 +1,73 @@
+//Location thing validator
+import {Review} from "../../src/types";
+import {validateReview} from "../../src/validators/reviewValidator";
+
+test("valid review - only score", () => {
+    let review:Review = {
+        markerId: "markerID-1234654fgh",
+        comment:undefined,
+        score: 4,
+        encodedPhoto: undefined
+    }
+    expect(validateReview(review));
+});
+
+test("valid review - only image", () => {
+    let review:Review = {
+        markerId: "markerID-1234654fgh",
+        comment:undefined,
+        score: undefined,
+        encodedPhoto: "encodedPhoto-5293084750924theiruh"
+    }
+    expect(validateReview(review));
+});
+
+test("valid review - only comment", () => {
+    let review:Review = {
+        markerId: "markerID-1234654fgh",
+        comment:"comment",
+        score: undefined,
+        encodedPhoto: undefined
+    }
+    expect(validateReview(review));
+});
+
+test("valid review - two elements", () => {
+    let review:Review = {
+        markerId: "markerID-1234654fgh",
+        comment:"comment",
+        score: 3,
+        encodedPhoto: undefined
+    }
+    expect(validateReview(review));
+});
+
+test("valid review - all elements", () => {
+    let review:Review = {
+        markerId: "markerID-1234654fgh",
+        comment:"comment",
+        score: 3,
+        encodedPhoto: "encodedPhoto-2093485iughsidg"
+    }
+    expect(validateReview(review));
+});
+
+test("invalid review - no elements", () => {
+    let review:Review = {
+        markerId: "markerID-1234654fgh",
+        comment:undefined,
+        score: undefined,
+        encodedPhoto: undefined
+    }
+    expect(validateReview(review));
+});
+
+test("valid review - no marker id", () => {
+    let review:Review = {
+        markerId: "    ",
+        comment:"comment",
+        score: 3,
+        encodedPhoto: "encodedPhoto-2093485iughsidg"
+    }
+    expect(validateReview(review));
+});
