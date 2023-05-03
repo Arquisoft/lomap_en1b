4e9e6cee05f6080f42cfc1c5670d0e9b1409199c
Iván Vega García <uo276670@uniovi.es>
2023-05-03 02:04:56 +0200

    Changed tets urls

--- webapp/src/__tests__/Map.test.tsx
+++ webapp/src/__tests__/Map.test.tsx
@@ -9,7 +9,7 @@ import {MapContainer} from "react-leaflet";
 
 const postResolver = jest.fn()
 const handlers = [
-    rest.get('http://localhost:8082/location', (req, res, ctx) => {
+    rest.get('http://api.lomap.mariopdev.com/location', (req, res, ctx) => {
         return res(
             ctx.status(200),
             ctx.json([
@@ -34,7 +34,7 @@ const handlers = [
             ]),
         )
     }),
-    rest.post('http://localhost:8082/location', (req, res, ctx) => {
+    rest.post('http://api.lomap.mariopdev.com/location', (req, res, ctx) => {
         return res(postResolver);
     })
 ]
