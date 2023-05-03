0f017331c1190c9b4f01d71c7f8967382805e13a
Elías Llera García-Riaño <eliaslleragr@gmail.com>
2023-05-03 02:09:16 +0200

    Cleaned up project and added restAPI coverage

--- restapi/jest.config.ts
+++ restapi/jest.config.ts
@@ -1,3 +1,15 @@
 module.exports = {
-    preset: 'ts-jest'
+    preset: 'ts-jest',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'build/src/**/*.js',
+        'build/!src/**/*.test.js',
+        'build/!src/index.js',
+    ],
+    coverageReporters: [
+        'json',
+        'lcov',
+        'text',
+        'text-summary',
+    ],
 };
